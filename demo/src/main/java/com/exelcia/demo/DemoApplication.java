130c70ab2fcc11ff987f1d6cd53c98216cd3d0dd
Vincent Leclerc <vleclerc@inow.fr>
2024-06-18 14:25:56 -0400

    app.yaml

--- demo/src/main/java/com/exelcia/demo/DemoApplication.java
+++ demo/src/main/java/com/exelcia/demo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		String db = System.getenv("DATABASE_URL");
+		System.out.println("DATABASE URL: " + db);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
